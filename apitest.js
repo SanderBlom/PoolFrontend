b64167cfa17797fc82d4d1be4c437f2e450d6373
sander Blomvågnes <sander@adaptic.no>
2022-02-25 16:49:58 +0100

    test

--- apitest.js
+++ apitest.js
@@ -1,3 +1,8 @@
+//This file is used to store functions related to fetching data from the pool table. 
+const moment = require('moment'); //Used to generate timestamps
+const fetch = require("node-fetch") //Used to fetch data from http/https
+const https = require("https"); //Used to be able to work with self signed certificates 
+let db = require("./db.js") //Gives us access to the db class to access data in the database
 
 
 async function CheckTableAvailability() {
