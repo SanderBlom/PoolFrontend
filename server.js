912056777248211c966456b679c76b5aa2cd2a58
sander Blomvågnes <sander@adaptic.no>
2022-03-23 16:32:09 +0100

    Redid the frontpage

--- server.js
+++ server.js
@@ -224,8 +224,6 @@ app.post("/user/dashboard/newgame", checkAuth, async (req, res) => {
         console.log(error)
     }
 
-    console.log('The table status is: ' + tableAvailability)
-
     if (tableAvailability == true) {
         gameid = await db.CreateNewGame(tableid) //Creating a game and returning the game id.
     }
