4acfb051e099f28a858605c93a2d360d930d1431
sander Blomvågnes <sander@adaptic.no>
2022-03-24 11:54:15 +0100

    Fixed wrong logic operator for gamestart

--- server.js
+++ server.js
@@ -286,9 +286,9 @@ app.post("/game/start/:id", checkAuth, async (req, res) => {
             tableid = await db.GetTableID(gameid) //Fetches the tableid for the game
             tablestatus = await vision.CheckTableAvailability(tableid)
             console.log('Table status = ' + tablestatus)
-            if(tablestatus != true){
+            if(tablestatus == true){
                 console.log('Gameid= ' + gameid + ' pID 1= ' + playerid1 + ' pID 2=' + playerid2 + ' usrName1= ' + username1 + ' usrName2= ' + username2 + 'timestamp ' + startgametime)
-                vision.SendStart(gameid,playerid1, playerid2, username1, username2,startgametime)
+                //vision.SendStart(gameid,playerid1, playerid2, username1, username2,startgametime)
             }
 
         }
