8b6b5a078f2e8769d4c5b84f7eca0f5185998cb4
sander Blomvågnes <sander@adaptic.no>
2022-05-03 14:17:03 +0200

    Removed apitest from git

--- apitest.js
+++ apitest.js
@@ -7,37 +7,6 @@ const { pool } = require("./dbConfig");
 
 
 
-async function CreateArray() {
-  let gameid = 84
-  let array = await db.GetAllBallPositions(gameid)
-  let balls = []
-  let max = 0
-  //Finding the lagrest playcount from the array
-  for (let index = 0; index < array.length; index++) {
-    const row = array[index];
-    if (row.playcount > max) {
-      max = row.playcount
-    }
-  }
-
-  //Creating rows in the object
-  for (let x = 1; x < max + 1; x++) {
-    balls.push([])
-
-  }
-  //Adding x, y and color to correct place in object
-  for (let index = 0; index < array.length; index++) {
-    const playcount = array[index].playcount - 1
-    const x_pos = array[index].x_pos
-    const y_pos = array[index].y_pos
-    const ballcoulor = array[index].ballcoulor
-    balls[playcount].push({ x_pos: x_pos, y_pos: y_pos, ballcoulor: ballcoulor })
-  }
-
-}
-
-
-
 async function correctWinsnLosses() {
   const query = {
     text: 'SELECT playerid FROM public.player;'
